bc8c90df975d9baaf7e3ce8b606e96473dd3514b
rinaldi600 <rinaldih84@gmail.com>
2023-06-17 22:53:26 +0700

    Fix bug 6

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ $(function() {
         const boxContent = $('#boxContent');
         $.each(msg.categories, function(key, value) {
             elementHTML += `
-            <div style='background: url("${msg.categories[key].strCategoryThumb}"); background-origin: border-box; background-position: center; background-repeat: no-repeat; background-size: cover; width: 384px; position:relative; cursor: pointer; min-height: 384px;' class='rounded-lg shadow-[0_3px_8px_0px_rgba(99,99,99,0.2)] overflow-hidden'>
-                <div style='position: absolute; display:flex; width:100%; bottom: 0; min-height: 100px; align-items: center; justify-content: center; padding: 2px; ' class="detail-category bg-black/50">
+            <div style='background: url("${msg.categories[key].strCategoryThumb}"); background-origin: border-box; background-position: center; background-repeat: no-repeat; background-size: cover; width: 384px; position:relative; cursor: pointer; min-height: 384px; border-radius: 0.5rem; overflow: hidden; box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;'>
+                <div style='position: absolute; display:flex; width:100%; bottom: 0; min-height: 100px; align-items: center; justify-content: center; padding: 2px; background-color: rgba(0,0,0,.5);' class="detail-category">
                     <input type="hidden" value="${msg.categories[key].strCategory
                     }">   
-                    <h1 class="hover:underline hover:decoration-2 text-white break-words font-semibold text-3xl">${msg.categories[key].strCategory
+                    <h1 style='color: rgb(255 255 255); overflow-wrap: break-word; font-weight: 600; font-size: 1.875rem; line-height: 2.25rem;'>${msg.categories[key].strCategory
                     }</h1>
                 </div>
             </div>
@@ -42,15 +42,14 @@ $(function() {
                   request.done(function( msg ) {
                     $.each(msg.meals, function(key, value) {
                         elementHTML += `
-                        <div style='background: url("${msg.meals[key].strMealThumb}"); background-origin: border-box; background-position: center; background-repeat: no-repeat; background-size: cover; width: 384px; position:relative; cursor: pointer; min-height: 384px;' class='rounded-lg shadow-[0_3px_8px_0px_rgba(99,99,99,0.2)] overflow-hidden'>
-                        <div style='position: absolute; display:flex; width: 100%; bottom: 0; min-height: 100px; align-items: center; justify-content: center;' class="detail-category p-2 bg-black/50">
-                                <h1 class="hover:underline hover:decoration-2 text-white break-words font-semibold text-3xl">${msg.meals[key].strMeal}</h1>
+                        <div style='background: url("${msg.meals[key].strMealThumb}"); background-origin: border-box; background-position: center; background-repeat: no-repeat; background-size: cover; width: 384px; position:relative; cursor: pointer; min-height: 384px; border-radius: 0.5rem; box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px; overflow: hidden;'>
+                        <div style='position: absolute; display:flex; width: 100%; bottom: 0; min-height: 100px; align-items: center; justify-content: center; padding: 2px; background-color: rgba(0,0,0,.5);' class="detail-category">
+                                <h1 style='color: rgb(255 255 255); overflow-wrap: break-word; font-weight: 600; font-size: 1.875rem; line-height: 2.25rem;'>${msg.meals[key].strMeal}</h1>
                             </div>
                         </div>
                         `
                     })
                     boxContent.html(elementHTML);
-                    const listFilterFood = $('#listFilter');
                     const titleListFilterFood = $('#title');
                     window.scrollTo(0,0);
                     $('#banner').remove();
